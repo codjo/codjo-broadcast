f0b96d37cf785805037956d5c036e4ab9a7d560f
CREGO <externe.sebastien.crego@allianzgi.fr>
2012-07-23 16:42:09 +0200

    Internationalisation SelectDateStep

--- codjo-broadcast-gui/src/main/java/net/codjo/broadcast/gui/plugin/DateBroadcastWizardCustomizer.java
+++ codjo-broadcast-gui/src/main/java/net/codjo/broadcast/gui/plugin/DateBroadcastWizardCustomizer.java
@@ -1,11 +1,20 @@
 package net.codjo.broadcast.gui.plugin;
 import net.codjo.broadcast.gui.wizard.SelectDateStep;
 import net.codjo.gui.toolkit.wizard.Wizard;
+import net.codjo.mad.gui.framework.GuiContext;
 /**
  *
  */
 public class DateBroadcastWizardCustomizer implements BroadcastWizardCustomizer {
+    private GuiContext guiContext;
+
+
+    public DateBroadcastWizardCustomizer(GuiContext guiContext) {
+        this.guiContext = guiContext;
+    }
+
+
     public void customizeWizard(Wizard wizard) {
-        wizard.addStep(new SelectDateStep());
+        wizard.addStep(new SelectDateStep(guiContext));
     }
 }
